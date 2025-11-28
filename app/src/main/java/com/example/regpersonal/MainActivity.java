a31ec9e1b70e0de5c9ecd9fb3682c01aff2bcf2b
Citrandeyelde <adrianrojas100@gmail.com>
2025-11-28 10:25:06 -0500

    Agregando Nuevos Activity

--- app/src/main/java/com/example/regpersonal/MainActivity.java
+++ app/src/main/java/com/example/regpersonal/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.regpersonal;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,16 +16,40 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
 
     Button btnConsultar, btnGuardar;
+    EditText txtDni,txtNombre,txtCorreo,txtCelular;
+    CheckBox chkCasa,chkDepa,chkAuto;
+    RadioButton rbMujer,rbHombre;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        btnConsultar=findViewById(R.id.btnConsultar);
+        btnGuardar=findViewById(R.id.btnGuardar);
+        txtDni=findViewById(R.id.txtDni);
+        txtNombre=findViewById(R.id.txtNombre);
+        txtCorreo=findViewById(R.id.txtCorreo);
+        txtCelular=findViewById(R.id.txtCelular);
+        chkCasa=findViewById(R.id.chkCasaPropia);
+        chkDepa=findViewById(R.id.chkDepa);
+        chkAuto=findViewById(R.id.chkAuto);
+        rbMujer=findViewById(R.id.rbMujer);
+        rbHombre=findViewById(R.id.rbHombre);
+
+        btnConsultar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
         });
+
+        btnGuardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
